cb374ce31507cfda72260d9df7b61fa036ea8eb0
Wayne Wu <wayne-wu@outlook.com>
2022-11-11 18:19:25 +0800

    feat: init

---
+++ netlify/core/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod config;
+pub mod error;
+pub mod response;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
