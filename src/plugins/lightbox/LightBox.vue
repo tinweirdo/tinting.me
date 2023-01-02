30cc60e67d778d4d01ef733aa52442d326287536
Wayne Wu <wayne-wu@outlook.com>
2023-01-03 00:04:59 +0800

    fix: lightbox stretch

--- src/plugins/lightbox/LightBox.vue
+++ src/plugins/lightbox/LightBox.vue
@@ -6,7 +6,7 @@ export default {
 
 <script lang="ts" setup>
 import { onKeyStroke, useWindowScroll } from '@vueuse/core'
-import { getCurrentInstance, watch } from 'vue'
+import { getCurrentInstance, nextTick, watch } from 'vue'
 import IconClose from '~icons/clarity/window-close-line'
 import { setInstance } from './index'
 let visible = $ref(false)
@@ -16,10 +16,13 @@ let caption = $ref('')
 const open = (_src: string, _caption?: string) => {
   src = _src
   caption = _caption ?? ''
-  visible = true
+  nextTick(() => visible = true)
 }
 
-const close = () => visible = false
+const close = () => {
+  visible = false
+  src = caption = ''
+}
 
 const { y } = useWindowScroll()
 
@@ -29,21 +32,25 @@ defineExpose({ open, close })
 
 setInstance(getCurrentInstance())
 
-onKeyStroke('Escape', () => visible = false)
+onKeyStroke('Escape', close)
 </script>
 
 <template>
   <teleport to="body">
     <div class="lightbox prose z-40 fixed top-0 left-0 bottom-0 right-0 flex items-center justify-center opacity-0 transition-opacity duration-300 pointer-events-none bg-bg-base" :class="{ 'opacity-100 pointer-events-auto': visible }">
       <figure class="!mb-0">
-        <img :src="src" class="max-w-80vw max-h-80vh cursor-zoom-out" @click="visible = false">
+        <img
+          class="max-w-80vw max-h-80vh cursor-zoom-out object-contain"
+          :src="src"
+          @click="close"
+        >
         <figcaption v-if="caption" class="text-center italic !mt-2em absolute bottom-6em w-full left-0" v-html="caption" />
       </figure>
       <IconClose
         class="absolute top-1.5em right-1.5em cursor-pointer"
         width="28"
         height="28"
-        @click="visible = false"
+        @click="close"
       />
     </div>
   </teleport>
